42b0e51cfc5aa73db127ac10889bf081ea067124
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-05-04 00:26:55 +0900

    05_04_study

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,13 @@ import cors from 'cors';
 import restaurants from './api/restaurants.route.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1/restaurants');
+app.use('/api/v1/restaurants', restaurants);
+app.use('*', (req, res) => {
+  res.status(404).json({ error: 'not found' });
+});
+
+export default app;
