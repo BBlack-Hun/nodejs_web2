6dd7f1189ec96ca7ae3854e786b0d49a5ddcfde9
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-05-04 00:07:51 +0900

    05_04_study

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,4 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+app.use('/api/v1/restaurants');
