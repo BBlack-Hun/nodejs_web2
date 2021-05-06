d54d46124345f5c4b566320e4f27e502542870f6
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-05-06 23:11:18 +0900

    05_06_study

--- frontend/src/components/restaurants.js
+++ frontend/src/components/restaurants.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import RestaurantDataService from '../services/restaurant';
+import { Link } from 'react-router-dom';
 
-function Restaurants() {
+const Restaurant = (props) => {
+  const initialRestaruantState = {
+    id: null,
+    name: '',
+    address: {},
+    cuisine: '',
+    revies: [],
+  };
+  const [restaurant, setRsetaurant] = useState(initialRestaruantState);
+
+  const getRestaurant = (id) => {
+    RestaurantDataService.get(id)
+      .then((response) => {
+        setRsetaurant(response.data);
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
   return <div className="App">Hello World.</div>;
-}
+};
 
-export default Restaurants;
+export default Restaurant;
