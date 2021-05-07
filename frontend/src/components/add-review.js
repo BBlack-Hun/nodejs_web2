eaa6d7e486faec6563f002399eef3394d22a37ea
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-05-07 17:36:02 +0900

    05_07_study

--- frontend/src/components/add-review.js
+++ frontend/src/components/add-review.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import RestaurantDataService from '../services/restaurant';
+import { Link } from 'react-router-dom';
 
-function AddReivew() {
+const AddReivew = (props) => {
+  let initialReviewState = '';
+
+  let editing = false;
+
+  if (props.location.state && props.location.state.currentReview) {
+    editing = true;
+    initialReviewState = props.location.state.currentReview.text;
+  }
+
+  const [review, setReview] = useState(initialReviewState);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = (event) => {
+    setReview(event.target.value);
+  };
+
   return <div className="App">Hello World.</div>;
-}
+};
 
 export default AddReivew;
